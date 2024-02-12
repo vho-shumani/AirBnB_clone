7641bd15442f7c439c3ddda552f603517172fbb0
Shumani Simeli <shumanisimeli@gmail.com>
2024-02-12 05:06:51 +0200

    updated user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Module defines the User class"""
+"""Module defines the User class that inherets from the BaseModel class"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Classe manage user objects"""
+    """Classe manage user objects
+
+    Attribute:
+    email(str)
+    password(str)
+    first_name(str)
+    last_name(str)
+    """
 
     email = ''
     password = ''
