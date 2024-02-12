e353356d0f3f1e4454aa1c2d3656d06611a65cf4
Shumani Simeli <shumanisimeli@gmail.com>
2024-02-12 03:34:32 +0200

    created user.py

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module defines the User class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Classe manage user objects"""
+    
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
